f3f7fcc645605a62e8877aa6b3256a640ef0a27e
MaartenHaine <maarten.haine@student.kuleuven.be>
2023-10-12 17:55:04 +0200

    Debugging (be sure that werkzeug version is on 2.3.7)

--- website/models.py
+++ website/models.py
@@ -10,7 +10,7 @@ class Note(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     username = db.Column(db.String(150))
