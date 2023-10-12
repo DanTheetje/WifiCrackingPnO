ab5ffb22cb40849696a38073744c79b3d46888b4
MaartenHaine <maarten.haine@student.kuleuven.be>
2023-10-12 17:38:15 +0200

    models add

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,30 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'abc' #Dit zorgt voor een encryptie van de data cookies en sessie data, je kan dit ook gwn disablen als het problemen veroorzaakt
-    
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+
     from .views import view
     from .auth import auth
 
     app.register_blueprint(view, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+    create_database(app)
+
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created Database')
