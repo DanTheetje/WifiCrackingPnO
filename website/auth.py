a5699b54b18316c2710108bb5c221ed69546982d
MaartenHaine <maarten.haine@student.kuleuven.be>
2023-10-13 23:50:17 +0200

    ability to sign in and log out

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,36 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
-    return render_template('login.html')
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if user.password == password: #als hash gebruikt: check_password_hash(user.password, password)
+                flash('You have Logged in!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('view.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('username does not exist.', category='error')
+
+    return render_template('login.html', user=current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return render_template('')
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/signup', methods= ['GET', 'POST'])
 def sign_up():
@@ -20,11 +40,23 @@ def sign_up():
         username = request.form.get('username')
         password = request.form.get('password')
         
+        user = User.query.filter_by(username=username).first()
+        usermail = User.query.filter_by(email=email).first()
         print(password, email, username)
-        if len(password) < 7:
+        if user:
+            flash('This username already exists!', category='error')
+        elif usermail:
+            flash('This email has already been used.', category='error')
+        elif len(password) < 7:
             flash('Make the password at least 7 characters long', category='error')
         else:
+            new_user = User(email=email, username=username, password=password) #If we want to encrypt the password in the database replace password=generate_password_hash(password, method="sha256")
+            db.session.add(new_user)
+            db.session.commit()
+            login_user(new_user, remember=True)
             flash('Account has been created', category='success')
+            return redirect(url_for('view.home'))
+            
 
-    return render_template('sign_up.html')
+    return render_template('sign_up.html', user=current_user)
 
--- website/views.py
+++ website/views.py
@@ -1,7 +1,21 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
+from flask_login import login_required, current_user
+from .models import Note
+from . import db
 
 view = Blueprint('view', __name__)
 
-@view.route('/')
+@view.route('/', methods=['GET', 'POST'])
+@login_required
 def home():
-    return render_template('home.html')
+    if request.method == 'POST':
+        note = request.form.get('note')
+        if len(note) <= 1:
+            flash('Note is too short', category='error')
+        else:
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added!', category='success')
+
+    return render_template('home.html', user=current_user)
