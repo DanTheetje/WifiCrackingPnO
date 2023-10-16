110c7dd07b1d672c29baaf3e00836ff6e99f11a6
timobn <bulow.timon1@fmail.com>
2023-10-16 18:03:34 +0200

    sniffer2

---
+++ mysniffer.py
@@ -0,0 +1,44 @@
+import sys
+from scapy.all import *
+import time
+
+def packet_list(packets, aantal):
+    print("____________________________________________________________________")
+    print("")
+    for x in range(0,aantal):
+        print(x+1,": ",packets[x])
+    print("")
+    print("____________________________________________________________________")
+    print("")
+
+def exam(packets, aantal):
+    packet_list(packets, aantal)
+    x = None
+    while x != -2 :
+        x = int(input("explore packet: "))-1
+        if x > -1:
+            packet = packets[x]
+            t = None
+            while t != "0":
+                print("")
+                print(packet.summary())
+                t = input("s(show) / hd(hexdump): ")
+                print("")
+                if t == "s":
+                    packet.show()
+                if t == "hd":
+                    hexdump(packet)
+                if t == "hr":
+                    hexraw(packet)
+            packet_list(packets, aantal)
+        if x == -1:
+            hoofd()
+
+def hoofd():
+    aantal = int(input("packets: "))
+    packets = sniff(count=aantal, filter="host 163.182.194.25", prn=lambda x: x.summary())
+    exam(packets, aantal)
+
+hoofd()
+
+#163.182.194.25
