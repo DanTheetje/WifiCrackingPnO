9572a9e297ab4e4aa94fbef1c9741d595f3c0358
MaartenHaine <maarten.haine@student.kuleuven.be>
2023-10-30 15:55:12 +0100

    forum additions

---
+++ website/static/index.js
@@ -0,0 +1,17 @@
+function deleteNote(noteId) {
+  fetch("/delete-note", {
+    method: "POST",
+    body: JSON.stringify({ noteId: noteId }),
+  }).then((_res) => {
+    window.location.href = "/";
+  });
+}
+
+function deletePost(postId) {
+  fetch("/delete-post", {
+    method: "POST",
+    body: JSON.stringify({ postId: postId }),
+  }).then((_res) => {
+    window.location.href = "/posts";
+  });
+}
