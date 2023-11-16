3d7e0286ab84b3acd9c7e845e68f2bd7eda6bc9b
MaartenHaine <maarten.haine@student.kuleuven.be>
2023-11-16 16:55:52 +0100

    last additions

--- website/static/index.js
+++ website/static/index.js
@@ -1,9 +1,17 @@
-
 function deletePost(postId) {
   fetch("/delete-post", {
     method: "POST",
     body: JSON.stringify({ postId: postId }),
   }).then((_res) => {
     window.location.href = "/post";
+  });
+}
+
+function donation(postId) {
+  fetch("/donate", {
+    method: "POST",
+    body: JSON.stringify({ postId: postId }),
+  }).then((_res) => {
+    window.location.href = "/forum";
   });
 }
