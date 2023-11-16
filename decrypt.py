a94e61708173018a8951f3e4217e597b6dce755e
timobn <bulow.timon1@fmail.com>
2023-11-16 14:22:08 +0100

    sniffer

--- decrypt.py
+++ decrypt.py
@@ -1,7 +1,7 @@
 import base64
 
-data = base64.b64decode("ff e2 c1 54 cd be 4a 8a f6 d0 3d 7e ba da bf f4 46 b6 82 85 18 f1 03 9d 96 94 08 02 28")
-key = "63 72 79 70 74 69 84"
+data = base64.b64decode("")
+key = ""
 
 S = range(256)
 j = 0
