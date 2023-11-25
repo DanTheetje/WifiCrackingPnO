d8f47d069dfc1e0da1942f6f41ccc04fcaa92650
dantheetje <dantekampioen29@gmail.com>
2023-11-25 16:40:55 +0100

    Decryption werkt

--- Sniffer/decrypt.py
+++ Sniffer/decrypt.py
@@ -1,61 +1,67 @@
 import scapy.all as scapy
 
-
-WEP_packets = scapy.rdpcap("Sniffer/WEPdummyPacket.pcap") # dit kan later problemen veroorzaken
-for packet in WEP_packets:
-    WEP_packet = packet
-
-# stap 1: IV, data, key vinden
-IV_binary = WEP_packet[scapy.Dot11WEP].iv
-
-WEPdata_binary = bytearray(WEP_packet.wepdata)
-
-#scapy.hexdump(WEP_packet)
-print(WEP_packet.wepdata.hex())
-print(str(WEP_packet[scapy.Dot11WEP].iv.hex()))
 # Function to convert ASCII to hex
 def ascii_to_hex(text):
     hex_result = ''.join(hex(ord(char))[2:] for char in text)
     return hex_result
 
 
-def generate_key_stream(ppk, iv, wepdata):
-    #PPK moet in bits staan
-    S = range(256)
+def generate_key_stream(ppk, wepdata):
+
+    #PPK staat in bytes, dit zijn 8 bytes
+    S = [x for x in range(256)] #(0,1,2,3,..255)
     j = 0
-    out = bytearray()
-    lengte_bits = len(ppk)*8
+    key_stream = bytearray()
+
+    #ik begrijp nog niet goed waarom 256 =2^8 ?= bits_in_ppk = len(ppk)*8?
     # KSA Phase
-    for i in range(256):
+    for i in range(256): #
         j = (j + S[i] + ppk[i % len(ppk)]) % 256
         S[i], S[j] = S[j], S[i]
 
     # PRGA Phase
     i = j = 0
-    for char in data:
+    for i in range(len(wepdata)):
         i = (i + 1) % 256
         j = (j + S[i]) % 256
         S[i], S[j] = S[j], S[i]
-        out.append(chr(ord(char) ^ S[(S[i] + S[j]) % 256]))
+        key_stream.append(S[(S[i] + S[j]) % 256]) #chr(ord(char) ^ S[(S[i] + S[j]) % 256]))
 
-    return out
+    return key_stream
 
 
-def wep_decrypt(ppk, iv, ciphertext):
-    key_stream = generate_key_stream(ppk, iv, ciphertext)
+def wep_decrypt(ppk, ciphertext):
+
+    key_stream = generate_key_stream(ppk, ciphertext)
     plaintext = bytearray()
 
     for i in range(len(ciphertext)):
+
         plaintext.append(ciphertext[i] ^ key_stream[i])
 
     return bytes(plaintext)
 
-WEPkey_binary = bytes.fromhex(ascii_to_hex(input("wat is de key?"))) # dit is ascii
-print(WEPkey_binary.hex())
+
+# packet selecteren
+WEP_packets = scapy.rdpcap("Sniffer/WEPdummyPacket.pcap") # dit kan later problemen veroorzaken
+for packet in WEP_packets:
+    WEP_packet = packet
+print("WEP packet has been successfully read")
+# stap 1: IV, data, key vinden
+IV_binary = WEP_packet[scapy.Dot11WEP].iv
+
+WEPdata_binary = bytearray(WEP_packet.wepdata)
+
+
+print("Encrypted Data of packet : 0x"+WEP_packet.wepdata.hex())
+print("IV: 0x"+str(WEP_packet[scapy.Dot11WEP].iv.hex()))
+
+WEPkey_binary = bytes.fromhex(ascii_to_hex(input("What is the WEP key?"))) # dit is ascii
+#print(WEPkey_binary.hex())
 
 ppk = IV_binary+WEPkey_binary
-print(ppk)
-print(len(ppk))
+print("Per packet key: "+str(ppk))
 
-print(bytearray(ppk))
-print(wep_decrypt(ppk,IV_binary,WEPdata_binary))
+print("Decrypted package: "+str(wep_decrypt(ppk,WEPdata_binary))) # \x in de output betekend dat volgende 2 char hexadecimaal zijn
+# om alle tekens te vertalen: Zie tabel https://docs.python.org/3/reference/lexical_analysis.html#string-and-bytes-literals
+print("To translate scape sequences: https://docs.python.org/3/reference/lexical_analysis.html#string-and-bytes-literals")
