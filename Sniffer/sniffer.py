84efb2e05331363a124122625cb22e9b8b82463c
DanTheetje <51004498+DanTheetje@users.noreply.github.com>
2023-12-07 14:42:49 +0100

    Update sniffer.py
    Variabele namen verbeterd

--- Sniffer/sniffer.py
+++ Sniffer/sniffer.py
@@ -2,15 +2,18 @@ from scapy.all import *
 import decrypt
 
 pkl = PcapWriter("/home/kali/Desktop/WEPpackets.pcap", append = True, sync = True)
+router_MAC= "74:da:38:eb:6f:dc"
+WEP_key = "ESAT2"
+network_interface = "wlan1" #"wlan0"
 
-def test(p):
+def decryptAndFilter(p):
 	if p.haslayer(Dot11WEP):
-		dp = decrypt.decryptWEP(p,"ESAT2")
+		dp = decrypt.decryptWEP(p,WEP_key)
 		if "HTTP"in str(dp) or "HTML" in str(dp).upper() or "PASSWORD" in str(dp).upper(): #geen upper bij http
 			pkl.write(p)
 			print("\n"+10*"-------------------"+"\n")
 			return dp
 		
-print("sniffing started...")
-sniff(iface="wlan1", filter="ether src 74:da:38:eb:6f:dc or ether dst 74:da:38:eb:6f:dc", prn=test, store=0)
-#sniff(iface="wlan0", filter="ether src 74:da:38:eb:6f:dc or ether dst 74:da:38:eb:6f:dc", prn=test, store=0)
+print("Sniffing started...")
+sniff(iface=network_interface, filter="ether src "+router_MAC+" or ether dst "+router_MAC, prn=decryptAndFilter, store=0)
+
